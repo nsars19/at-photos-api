a42d0289801eb36f7ba4a1b6bed497b65158695e
Nick S <nsardonini@gmail.com>
2021-04-29 18:51:20 -0400

    remove populate method as the field type is a number not a ref

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get("/photos", async (req, res) => {
 app.get("/photos/:id", async (req, res) => {
   const { id } = req.params;
 
-  const photo = await Photo.findById(id).populate("likes");
+  const photo = await Photo.findById(id);
   res.send(photo);
 });
 module.exports = app;
