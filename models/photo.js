eba9f0c25cb0c0b9c9bc34af916657a175babb9e
Nick S <nsardonini@gmail.com>
2021-04-29 20:21:38 -0400

    add comment field to photo schema

--- models/photo.js
+++ models/photo.js
@@ -7,6 +7,7 @@ const PhotoSchema = new Schema({
     type: Number,
     default: 0,
   },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
 });
 
 module.exports = mongoose.model("Photo", PhotoSchema);
